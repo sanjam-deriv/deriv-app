76f92a103446f938d9e5a47f3c3bbde6c630d422
sanjam chhatwal <sanjam@deriv.com>
2022-11-15 16:37:43 +0400

    Data table migration

---
+++ packages/components/src/components/data-table/index.ts
@@ -0,0 +1,5 @@
+import DataTable from './data-table';
+import './data-table.scss';
+
+
+export default DataTable;
--- packages/components/src/components/types/common.types.ts
+++ packages/components/src/components/types/common.types.ts
@@ -3,3 +3,6 @@ import React from 'react';
 export type TGenericObjectType = {
     [key: string]: React.ReactNode;
 };
+
+
+export type TTableRowItem = { component: React.ReactNode } | string
